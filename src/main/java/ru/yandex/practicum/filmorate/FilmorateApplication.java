8b11ca40ecc447e311711b9ba3144360fe39c3ee
Naritab_ <rzekicc@gmail.com>
2025-09-09 20:03:15 +0600

    Delete unused exception

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,11 +3,9 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class FilmorateApplication {
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
 }
