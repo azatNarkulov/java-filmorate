5f9e19200edb0a97022ec77efa095dae26e7f238
Naritab_ <rzekicc@gmail.com>
2025-09-09 19:51:09 +0600

    Fix annotation in one line

--- src/main/java/ru/yandex/practicum/filmorate/model/film/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/film/Genre.java
@@ -3,7 +3,8 @@ package ru.yandex.practicum.filmorate.model.film;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter @AllArgsConstructor
+@Getter
+@AllArgsConstructor
 public class Genre {
     private final int id;
     private final String name;
